9897f8e056411502bce74dc85e189bc8557987d1
sajera <allsajera@gmail.com>
2017-10-26 12:57:18 +0300

    * code preparing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// eslint-disable import/first
 
 // outsource dependencies
 import React from 'react';
@@ -5,33 +6,41 @@ import thunk from 'redux-thunk';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+// import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+// STYLES inject ...
+import './style';
 
 // local dependencies
-import './style/all.css';
 import reducers from './reducers';
+// import actions from './actions';
+// import components from './components';
+// import constants from './constants';
 
 // configuration
 // Note: this API requires redux@>=3.1.0
-// var store = createStore(
-//   reducers,
-//   applyMiddleware(thunk)
-// );
+var store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+);
+
+// process.env.SOME
 
 var createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 console.log('root', document.getElementById('root'));
 ReactDOM.render(
-    <p> Its will be Flask admin application </p>
-    // <Provider store={ createStoreWithMiddleware(reducers) }>
-    // <Provider store={ createStore(reducers, applyMiddleware(thunk) ) }>
-    //     <Router history={ browserHistory }>
-    //         <Route path="/" component={ App }>
-    //             <Route path="/signin" component={ SignIn }></Route>
-    //             <Route path="/signup"></Route>
-    //             <Route path="/feature"></Route>
-    //         </Route>
-    //     </Router>
-    // </Provider>
+    <div>
+            <p> Its will be Flask admin application </p>
+            {/* <Provider store={ store }>
+                    <Router history={ browserHistory }>
+                        <Route path="/" component={ App }>
+                            <Route path="/signin" component={ SignIn }></Route>
+                            <Route path="/signup"></Route>
+                            <Route path="/feature"></Route>
+                        </Route>
+                    </Router>
+                </Provider> */}
+    </div>
     ,
     document.getElementById('root')
 );
---
+++ src/style/index.js
@@ -0,0 +1,11 @@
+
+// import with fonts ... 
+// TODO: try to make override for variables
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+// app styles (result of preprocessor less)
+import './all.css';
+// TODO: try to connect something else ...
+
+export default {'connect': 'styles'};
