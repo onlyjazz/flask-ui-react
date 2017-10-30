e399d910204ccd42542333e549de0bf2aa1865c6
sajera <allsajera@gmail.com>
2017-10-30 20:04:37 +0200

    * page change action update

--- src/actions/page-change.js
+++ src/actions/page-change.js
@@ -4,10 +4,10 @@
 // local dependencies
 import { DATA_PAGE_CHANGE } from './types';
 
-export function pageChange ( url ) {
+export function pageChange ( location ) {
 
     return {
         type: DATA_PAGE_CHANGE,
-        url,
+        location,
     }
 }
