8fa384f2cba69e98dbce9ef34f0bc607565e5cd3
sajera <allsajera@gmail.com>
2017-11-21 19:00:13 +0200

    + form input component

---
+++ src/components/form-input.js
@@ -0,0 +1,20 @@
+
+// outsource dependencies
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+// local dependencies
+
+// configuration
+
+export default function ({ input, label, meta: { touched, error }, ...custom }) {
+    return (
+        <TextField
+            {...input}
+            {...custom}
+            fullWidth={true}
+            hintText={label}
+            floatingLabelText={label}
+            errorText={touched && error}
+        />
+    )
+}
