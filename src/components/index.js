812c5c9d4451331bfb53e38a46a927a3d745d1e7
sajera <allsajera@gmail.com>
2017-11-21 19:01:16 +0200

    * connect new form component to the app

--- src/components/index.js
+++ src/components/index.js
@@ -5,4 +5,6 @@ export { default as Private } from './private';
 export { default as LogoBig } from './logo-big';
 export { default as SideMenu } from './side-menu';
 export { default as Preloader } from './preloader';
+export { default as FormInput } from './form-input';
+export { default as FormSelect } from './form-select';
 export { default as InputAddon } from './input-addon';
