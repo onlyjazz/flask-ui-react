54769b1ea7bde886a3664aff6311585eb8548a90
sajera <allsajera@gmail.com>
2017-11-23 15:37:41 +0200

    * config  -- added path to GraphQL

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,6 +10,8 @@ const configDev = {
     DEBUG: true,
     production: false,
     apiPath: 'http://flask.clearclinica.dev.dfusiontech.com:19080',
+    graphQlPath: 'http://flask.clearclinica.dev.dfusiontech.com:5004',
+    
 };
 
 export const config = configDev;
